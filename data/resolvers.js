9f06267a97b6374682acf885627de7aa3fe8950d
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-10-17 11:52:54 +0100

    add datetime scalar

--- data/resolvers.js
+++ data/resolvers.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { GraphQLScalarType } = require('graphql');
+const { Kind } = require('graphql/language');
 const { User, Post, Tag } = require('../models');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
@@ -191,9 +193,31 @@ const resolvers = {
         }
     },
 
-    DateTime: {
+    DateTime: new GraphQLScalarType({
+        name: 'DateTime',
+        description: 'DateTime type',
 
-    }
+        parseValue(value) {
+            // value from the client
+            return new Date(value);
+        },
+
+        serialize(value) {
+            const date = new Date(value);
+
+            // value sent to the client
+            return date.toISOString();
+        },
+
+        parseLiteral(ast) {
+            if (ast.kind === Kind.INT) {
+                // ast value is always in string format
+                return parseInt(ast.value, 10);
+            }
+
+            return null;
+        }
+    })
 };
 
 module.exports = resolvers;
